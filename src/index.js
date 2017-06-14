388428d5e5330a41bbc4c5e7d4353533538eaa4d
Alex Kaufman <alexkaufman06@gmail.com>
2017-06-13 22:05:56 -0700

    Highlight winner and prepare for mobile

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick} id={props.id}>
       {props.value}
     </button>
   );
@@ -16,6 +16,7 @@ class Board extends React.Component {
       <Square 
         value={this.props.squares[i]} 
         onClick={() => this.props.onClick(i)}
+        id={i}
         />
     );
   }
@@ -116,13 +117,16 @@ class Game extends React.Component {
       <div className="game">
         <div className="game-board">
           <div>
-            <h1 className="title-text">Tic-Tac-Toe</h1>
+            <h1 className="title-text">Tic-Toc-Toe</h1>
             <div className="status">{status}</div>
           </div>
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
+          <div className={test}>
+            <ol>{moves}</ol>
+          </div>
         </div>
         <div className={test}>
           <ol>{moves}</ol>
@@ -153,6 +157,9 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      document.getElementById(a).className = "winner";
+      document.getElementById(b).className = "winner";
+      document.getElementById(c).className = "winner";
       return squares[a];
     }
   }
